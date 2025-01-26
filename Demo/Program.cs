be50ba2fce1d97e8705f3b0ba3fd03fd54918203
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-01-26 14:09:59 +0200

    Video 01 - What is the LINQ?

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Video 01 - What is the LINQ
+            List<int> Numbers = new List<int>(10) {1,2,3,4,5,6,7,9};
+            List<int> OddNumbers = Numbers.Where(N=> N % 2 != 0).ToList();
+            foreach (var item in OddNumbers)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
         }
     }
 }
