ba9d641d5aba923152290fd767965bebfcad9286
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-01-27 11:24:03 +0200

    Q1 -  Find all products that are out of stock.

---
+++ Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using static Assignment.ListGenerator;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LINQ - Restriction Operators
+            #region Q1 -  Find all products that are out of stock.
+            //Fluent Syntax
+            var OutOfStock = ProductList.Where(P => P.UnitsInStock == 0);
+            //Query Syntax
+            OutOfStock = from P in ProductList
+                         where P.UnitsInStock == 0
+                         select P;
+            foreach(var item in OutOfStock)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+            #endregion
+        }
+    }
+}
