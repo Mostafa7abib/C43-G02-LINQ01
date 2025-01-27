cf9af91f99081a2f8ce4749f4fc9f359eb5e3071
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-01-27 11:37:05 +0200

    Q3 - Returns digits whose name is shorter than their value.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -19,13 +19,22 @@ namespace Assignment
             //}
             #endregion
             #region Q2 - Find all products that are in stock and cost more than 3.00 per unit.
-            //Fluent Syntax
-            var InStockAndCostMoreThan3 = ProductList.Where(P => P.UnitsInStock > 0 && P.UnitPrice > 3.00m);
-            //Query Syntax
-            InStockAndCostMoreThan3 = from P in ProductList
-                                      where P.UnitsInStock > 0 && P.UnitPrice > 3.00m
-                                      select P;
-            foreach(var item in InStockAndCostMoreThan3)
+            ////Fluent Syntax
+            //var InStockAndCostMoreThan3 = ProductList.Where(P => P.UnitsInStock > 0 && P.UnitPrice > 3.00m);
+            ////Query Syntax
+            //InStockAndCostMoreThan3 = from P in ProductList
+            //                          where P.UnitsInStock > 0 && P.UnitPrice > 3.00m
+            //                          select P;
+            //foreach(var item in InStockAndCostMoreThan3)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Q3 - Returns digits whose name is shorter than their value.
+            string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            //Fluent Syntax //valid here only
+            var NameShorterThanValue = Arr.Where((P, I) => P.Length < I);
+            foreach(var item in NameShorterThanValue)
             {
                 Console.WriteLine(item);
             }
